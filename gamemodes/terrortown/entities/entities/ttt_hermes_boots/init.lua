57f02448ace7bd231bb175f34eb49e055010d6cb
Zaratusa <zaratusa.codec@gmail.com>
2020-07-11 11:14:09 +0200

    feat: re-add automatic workshop download

--- gamemodes/terrortown/entities/entities/ttt_hermes_boots/init.lua
+++ gamemodes/terrortown/entities/entities/ttt_hermes_boots/init.lua
@@ -4,6 +4,8 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 
+resource.AddWorkshop("650523765")
+
 include('shared.lua')
 
 CreateConVar("ttt_hermesboots_detective_loadout", 0, FCVAR_ARCHIVE, "Should Detectives have the Hermes Boots in their loadout?", 0, 1)
